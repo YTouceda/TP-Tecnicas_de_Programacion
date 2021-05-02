44d3f75ac0be05f2d134ef8f866cc741011127ee
DanielPipet <danielpipet15@gmail.com>
2021-05-02 19:28:22 -0300

    Modulo3

---
+++ Modulo3_Equipo1a/Capa_BLL/OrdenDeVentaBLL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using General;
+
+namespace Capa_BLL
+{
+    public class OrdenDeVentaBLL
+    {
+        private OrdenDeVenta _ordenDeVenta;
+    }
+}
---
+++ Modulo3_Equipo1a/Capa_Consola/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using General;
+using Entidades;
+
+namespace Capa_Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion=1;
+            Usuario user = new Usuario();
+            Cliente unCliente = new Cliente();
+            OrdenDeVenta unaOrden;
+            List<DetalleOrden> detalles = new List<DetalleOrden>();
+            Tarjeta unaTarjeta;
+            //Console.WriteLine("Eligir una opcion: \n\n1) Realizar una venta.\n\n0)Salir.\n\n");
+            //opcion = int.Parse(Console.ReadLine());
+            if (opcion == 1)
+            {
+                Direccion dire = new Direccion("123", "calle", "1234", "Localidad", "provincia");
+                unCliente.Direccion = dire;
+                unCliente.Apellido = "perez";
+                unCliente.DNI = 11111111;
+                unCliente.Nombre = "Juan";
+                unaOrden = new OrdenDeVenta();
+                unaOrden.Cliente = unCliente;
+                unaOrden.UsuarioCreador = user;
+                unaOrden.Detalles = detalles;
+                //Console.WriteLine("\n\ningrese Metodo De Pago\n\n1)Tarjeta\n\n2)Efectivo\n\n");
+                //opcion = int.Parse(Console.ReadLine());
+                if (opcion==1)
+                {
+                    unaTarjeta = new Tarjeta("CVC", "fechaVencimiento", "nombreTarjeta", 1111111112);
+                    if (unaTarjeta.Validar()) 
+                    {
+                    unaOrden.MetodoDePago = unaTarjeta;
+                    }
+                }
+
+
+            }
+
+        }
+    }
+}
