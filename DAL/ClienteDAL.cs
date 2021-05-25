ef048c1cddd89f9095ec0462ca097fe90c33f1dd
lucas <lucasagustin592@gmail.com>
2021-05-24 22:33:10 -0300

    hola

--- DAL/ClienteDAL.cs
+++ DAL/ClienteDAL.cs
@@ -15,16 +15,34 @@ namespace DAL
         /// </summary>
         /// <param name="pCliente">Objeto cliente</param>
         /// <returns></returns>
-        public bool Alta(Entity.Cliente pCliente)
+        public static bool Alta(Entity.Cliente pCliente)
         {
             Conexion objConexion = new Conexion();
-            string query = string.Format("INSERT INTO Cliente(Id_Persona) VALUES({0})", pCliente.IDPersona);
+
+            string query = string.Format("INSERT INTO Direccion(Altura,Calle,CodPostal,Localidad,Provincia)VALUES ('{0}','{1}','{2}','{3}','{4}')", pCliente.Direccion.Altura, pCliente.Direccion.Calle, pCliente.Direccion.CodigoPostal, pCliente.Direccion.Localidad, pCliente.Direccion.Provincia);
+            objConexion.EscribirPorComando(query);
+            DataTable objDataTable = objConexion.LeerPorComando("SELECT IDENT_CURRENT ('Direccion') AS Id_Direccion;");
+            
+            query = string.Format("INSERT INTO Persona(Apellido,Dni,Nombre,Id_direccion)VALUES('{0}','{1}','{2}'", pCliente.Apellido, pCliente.DNI, pCliente.Nombre) + "," + (int)(objDataTable.Rows[0]["Id_Direccion"]) + ")";
+            objDataTable = objConexion.LeerPorComando("SELECT IDENT_CURRENT('Persona') AS Id_Persona");
+
+            query =string.Format("INSERT INTO Cliente(Id_Persona) VALUES({0})", (int)(objDataTable.Rows[0]["Id_Persona"]));
             objConexion.EscribirPorComando( query );
 
 
 
             return true;
      
+        }
+
+        public static bool QueryModificarCliente(Entity.Cliente mCliente)
+        {
+            Conexion objconexion = new Conexion();
+            string query = string.Format("UPDATE Direccion SET Altura = '{0}', Calle = '{1}', CodPostal = '{2}', Localidad = '{3}', Provincia = '{4}' WHERE Id_direccion = ",mCliente.Direccion.Altura,mCliente.Direccion.Calle,mCliente.Direccion.CodigoPostal,mCliente.Direccion.Localidad,mCliente.Direccion.Provincia) + mCliente.Direccion.Id_Direccion;
+            objconexion.EscribirPorComando(query);
+            query = string.Format("UPDATE Persona SET Apellido = '{0}', Dni = '{1}', Nombre = '{2}' WHERE Id_Persona = ",mCliente.Apellido,mCliente.DNI,mCliente.Nombre)+ mCliente.IDPersona;
+            objconexion.EscribirPorComando(query);
+            return true;
         }
         /// <summary>
         /// Comprueba si un cliente ya se encuentra en la base de datos.
