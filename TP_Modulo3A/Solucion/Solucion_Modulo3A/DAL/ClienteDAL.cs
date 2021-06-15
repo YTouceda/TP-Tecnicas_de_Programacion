e989a8065c1f9f5590b89494904e25aee750e4cf
salvador330 <salvadorcirino2004@hotmail.com>
2021-06-14 22:40:39 -0300

    update modifico query ClienteDAL

--- TP_Modulo3A/Solucion/Solucion_Modulo3A/DAL/ClienteDAL.cs
+++ TP_Modulo3A/Solucion/Solucion_Modulo3A/DAL/ClienteDAL.cs
@@ -22,7 +22,7 @@ namespace DAL
             Conexion objConexion = new Conexion();
             bool salida = true;
 
-            string query = string.Format("INSERT INTO direccion(altura,calle,codigo_postal,localidad,provincia)VALUES ('{0}','{1}','{2}','{3}','{4}')", pCliente.Direccion.Altura, pCliente.Direccion.Calle, pCliente.Direccion.CodigoPostal, pCliente.Direccion.Localidad, pCliente.Direccion.Provincia);
+            string query = string.Format("INSERT INTO direccion(calle,altura,localidad,codigo_postal,provincia)VALUES ('{0}','{1}','{2}','{3}','{4}')", pCliente.Direccion.Calle, pCliente.Direccion.Altura, pCliente.Direccion.Localidad, pCliente.Direccion.CodigoPostal, pCliente.Direccion.Provincia);
 
             if (objConexion.EscribirPorComando(query) == -1)
             {
@@ -33,7 +33,7 @@ namespace DAL
 
             DataTable objDataTable = objConexion.LeerPorComando("SELECT IDENT_CURRENT ('direccion') AS id_direccion;");
 
-            query = string.Format("INSERT INTO persona(apellido,dni,nombre,id_direccion)VALUES('{0}','{1}','{2}',{3})", pCliente.Apellido, pCliente.DNI, pCliente.Nombre, (objDataTable.Rows[0]["ID_DIRECCION"]));
+            query = string.Format("INSERT INTO persona(apellido,nombre,dni,id_direccion)VALUES('{0}','{1}','{2}',{3})", pCliente.Apellido, pCliente.Nombre, pCliente.DNI, (objDataTable.Rows[0]["id_direccion"]));
 
 
             if (objConexion.EscribirPorComando(query) == -1)
@@ -68,8 +68,7 @@ namespace DAL
             bool salida = true;
 
 
-            string query = string.Format("UPDATE direccion SET altura = '{0}', calle = '{1}', codigo_postal = '{2}', localidad = '{3}', provincia = '{4}' WHERE id_dreccion = {5}", mCliente.Direccion.Altura, mCliente.Direccion.Calle, mCliente.Direccion.CodigoPostal, mCliente.Direccion.Localidad, mCliente.Direccion.Provincia, mCliente.Direccion.ID);
-
+            string query = string.Format("UPDATE direccion SET calle = '{0}', altura = '{1}', localidad = '{2}', codigo_postal = '{3}', provincia = '{4}' WHERE id_dreccion = {5}", mCliente.Direccion.Calle, mCliente.Direccion.Altura, mCliente.Direccion.Localidad, mCliente.Direccion.CodigoPostal, mCliente.Direccion.Provincia, mCliente.Direccion.ID);
 
             if (objconexion.EscribirPorComando(query) == -1)
             {
@@ -77,9 +76,9 @@ namespace DAL
                 return salida;
             }
 
-            query = string.Format("UPDATE persona SET apellido = '{0}', dni= '{1}', nombre = '{2}' WHERE id_persona = ", mCliente.Apellido, mCliente.DNI, mCliente.Nombre) + mCliente.IDPersona;
-
-
+ 
+            query = string.Format("UPDATE persona SET apellido = '{0}', nombre= '{1}', dni = '{2}' WHERE id_persona = ", mCliente.Apellido, mCliente.Nombre, mCliente.DNI) + mCliente.IDPersona;
+          
             if (objconexion.EscribirPorComando(query) == -1)
             {
                 salida = false;
@@ -98,7 +97,7 @@ namespace DAL
         {
 
             Conexion objConexion = new Conexion();
-            string query = string.Format("SELECT C.id_cliente, P.id_persona, P.nombre, P.apellido, P.dni, D.id_direccion, D.calle, D.altura, D.codigo_postal, D.localidad, D.provincia FROM cliente C INNER JOIN persona P ON C.id_cliente = P.id_persona INNER JOIN direccion D ON P.id_direccion = D.id_direccion WHERE P.dni LIKE '%{0}%'", DNI);
+            string query = string.Format("SELECT C.id_cliente, P.id, P.nombre, P.apellido, P.dni, D.id, D.calle, D.altura, D.codigo_postal, D.localidad, D.provincia FROM cliente C INNER JOIN persona P ON C.id_cliente = P.id INNER JOIN direccion D ON P.id_direccion = D.id WHERE P.dni LIKE '%{0}%'", DNI);
             DataTable objDataTable = objConexion.LeerPorComando(query);
             if (objDataTable != null)
             {
