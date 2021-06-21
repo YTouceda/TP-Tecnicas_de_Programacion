f9544e754c023c0b3ec36ed95b7b91935f4dfe2e
YTouceda <Yannicktouceda@gmail.com>
2021-06-20 21:18:03 -0300

    delete

---
+++ TP_Modulo3A/BBDD/SP SELECT REPORTE VENTAS POR SEMANA.sql
@@ -0,0 +1,15 @@
+use [dbTecProg]
+go
+
+create proc [dbo].[sp_reporteventasporsemana] @fecha datetime, @fecha2 datetime
+as
+select o.[id]
+      ,[fecha]
+      ,o.[id_persona] as 'legajo_vendedor'
+	  ,pe.nombre +' '+ pe.apellido as 'nombre_vendedor'
+	  ,mp.tipo as 'metodo_pago'
+	  ,do.cantidad 'cantidad_producto'
+	  ,p.precio_venta as 'precio_producto'
+	  ,p.nombre as 'nombre_producto'
+	  
+  from [dbo].[orden] o inner join usuario u on u.legajo = o.id_persona inner join orden_venta ov on o.id = ov.id_orden inner join metodo_pago mp on ov.id_metodo_pago = mp.id_metodo_pago inner join detalle_orden do on o.id = do.id_orden inner join producto p on p.id_producto = do.id_producto inner join persona pe on pe.id= u.id_persona where o.FECHA between @fecha and @fecha2 
